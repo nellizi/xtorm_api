c128b8b3e17a5fb879da77a929b84e8f0760af4b
nellizi <masterliz9095@gmail.com>
2023-10-06 17:39:44 +0900

    Test : commit test

--- src/com/liz/appmain/Main.java
+++ src/com/liz/appmain/Main.java
@@ -10,6 +10,8 @@ public class Main {
 	
 	public static void main(String[] args) {
 		logger.log(Level.ALL,"LOG TEST");
+		logger.log(Level.ALL,"LOG TEST");
+		logger.log(Level.ALL,"LOG TEST");
 		
 	}
 
